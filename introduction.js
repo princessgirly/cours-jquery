bedb65f690a5c6ad2118adbaad5960a3873a8da8
cesi-RIL-2016-nadege <princessgirlyfactory@gmail.com>
2016-12-14 19:07:23 +0100

    Update introduction.js

--- introduction.js
+++ introduction.js
@@ -35,4 +35,24 @@
   });
 </script>
 
-//
+//De la même façon , vous pouvez ajouter des classes à un élément avec jQuery addClass()fonction, vous pouvez les supprimer avec jQuery removeClass()fonction.
+//Voici comment vous feriez cela pour un bouton spécifique:
+//Enlevons la btn-default classe de tous nos buttons éléments.
+
+$("#target2").removeClass("btn-default");
+
+<script>
+  $(document).ready(function() {
+    $("button").addClass("animated bounce");
+    $(".well").addClass("animated shake");
+    $("#target3").addClass("animated fadeOut");
+    $("button").removeClass("btn-default");
+  });
+</script>
+
+//Nous pouvons également changer le CSS d'un élément HTML directement avec jQuery.
+//jQuery a une fonction appelée .css()qui vous permet de modifier le CSS d'un élément.
+//Voici comment nous changer sa couleur au bleu:
+
+$("#target1").css("color", "blue");
+
