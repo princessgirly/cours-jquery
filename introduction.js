c10dffbc41598f530b43f5c75d969e32c8994566
cesi-RIL-2016-nadege <princessgirlyfactory@gmail.com>
2016-12-14 22:15:07 +0100

    Update introduction.js

--- introduction.js
+++ introduction.js
@@ -234,4 +234,24 @@ $(".target:even").addClass("animated shake");   //pair
     $(".target:nth-child(2)").addClass("animated bounce");
     $(".target:even").addClass("animated shake");
   });
-</script
+</script>
+
+//jQuery peut cibler l' bodyélément ainsi.
+//Voici comment nous ferions tout le corps fade out:
+$("body").addClass("animated fadeOut");
+
+<script>
+  $(document).ready(function() {
+    $("#target1").css("color", "red");
+    $("#target1").prop("disabled", true);
+    $("#target4").remove();
+    $("#target2").appendTo("#right-well");
+    $("#target5").clone().appendTo("#left-well");
+    $("#target1").parent().css("background-color", "red");
+    $("#right-well").children().css("color", "orange");
+    $("#left-well").children().css("color", "green");
+    $(".target:nth-child(2)").addClass("animated bounce");
+    $(".target:even").addClass("animated shake");
+    $("body").addClass("animated hinge");               //animation toute la page tombe et est supprimée   
+   });
+</script>
