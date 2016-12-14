3619e833e016ec2d7c49b1d4fa2ad823a2afd411
cesi-RIL-2016-nadege <princessgirlyfactory@gmail.com>
2016-12-14 18:30:15 +0100

    Update introduction.js

--- introduction.js
+++ introduction.js
@@ -4,3 +4,33 @@
   $(document).ready(function() {
   });
 </script>
+
+
+// jquery sélectionne souvent un élément html avec un selector ex faire rebondir des boutons
+
+<script>
+  $(document).ready(function() {
+    $("button").addClass("animated bounce");
+  });
+</script>
+
+//sélectionner tous les elements de la classe .text-primary et la classe .text-primary mettre animation (bouge) animated shake ou bounce
+
+<script>
+  $(document).ready(function() {
+    $("button").addClass("animated bounce");
+    $(".well").addClass("animated shake");
+    $(".text-primary").addClass("animated shake");
+  });
+</script>
+
+//#target3 disparait
+<script>
+  $(document).ready(function() {
+    $("button").addClass("animated bounce");
+    $(".well").addClass("animated shake");
+    $("#target3").addClass("animated fadeOut");
+  });
+</script>
+
+//
